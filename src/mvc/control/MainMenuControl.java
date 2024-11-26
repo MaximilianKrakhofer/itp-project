661dfba4586884ceaabae4452bec63a13c80e376
levi <148866368+l-konrad@users.noreply.github.com>
2024-11-26 13:49:13 +0100

    Code: MainMenuControl

--- src/mvc/control/MainMenuControl.java
+++ src/mvc/control/MainMenuControl.java
@@ -1,5 +1,31 @@
 package mvc.control;
+import mvc.model.MainMenuModel;
+import mvc.view.MainMenuView;
+import mvc.control.MainMenuControl;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainMenuControl {
 
+    MainMenuView view;
+    MainMenuModel model;
+    public MainMenuControl(MainMenuView view, MainMenuModel model) {
+        this.view = view;
+        this.model = model;
+
+    }
+    public static void main(String[] args) {
+        MainMenuView view = new MainMenuView("Main Menu");
+        MainMenuModel model = new MainMenuModel();
+        new MainMenuControl(view, model);
+    }
+
+    public class ButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
 }
