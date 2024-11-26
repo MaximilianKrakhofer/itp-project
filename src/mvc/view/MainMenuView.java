eed590092e7fe4355828b9e57d45d867692aae57
levi <148866368+l-konrad@users.noreply.github.com>
2024-11-26 13:25:56 +0100

    Code: MainMenu Sketch

--- src/mvc/view/MainMenuView.java
+++ src/mvc/view/MainMenuView.java
@@ -1,4 +1,64 @@
 package mvc.view;
 
-public class MainMenuView {
+import javax.swing.*;
+import java.awt.*;
+public class MainMenuView extends JFrame {
+    JTextArea cards;
+    JScrollPane cardsPane;
+    JButton add;
+    JButton delete;
+    JButton save;
+    JButton load;
+
+    public MainMenuView() {
+        JFrame frame = new JFrame();
+
+        frame.setSize(800, 600);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+
+        frame.add(panel);
+        frame.setVisible(true);
+        frame.pack();
+
+
+        panel.setLayout(new BorderLayout());
+
+        JPanel operations = new JPanel(new GridLayout(4, 1));
+        add = new JButton("Hinzufuegen");
+        delete = new JButton("Loeschen");
+        save = new JButton("Speichern");
+        load = new JButton("Laden");
+
+        operations.add(add);
+        operations.add(delete);
+        operations.add(save);
+        operations.add(load);
+
+        panel.add(operations, BorderLayout.NORTH);
+
+        cards = new JTextArea("");
+        cards.setEditable(false);
+        cards.setColumns(8);
+        cards.setRows(10);
+        cards.setEditable(false);
+        cards.setFont(new Font("Monospaced", Font.PLAIN, 30));
+        cardsPane = new JScrollPane(cards);
+        panel.add(cardsPane, BorderLayout.CENTER);
+
+        JPanel selectButtons = new JPanel(new GridLayout(3, 1));
+        JButton mainMenu = new JButton("MainMenu");
+        JButton quiz = new JButton("Quiz");
+        JButton hangman = new JButton("Hangman");
+        selectButtons.add(mainMenu);
+        selectButtons.add(quiz);
+        selectButtons.add(hangman);
+
+        panel.add(selectButtons, BorderLayout.SOUTH);
+
+    }
+
+
 }
