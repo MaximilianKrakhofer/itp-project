71717ebdc3b4d3aaa8aa59fe8ebd7b52a1a05779
MaximilianKrakhofer <mkrakhofer@student.tgm.ac.at>
2025-02-24 16:51:25 +0100

    Begin: Wordle Model

--- src/main/java/mvc/model/WordleModel.java
+++ src/main/java/mvc/model/WordleModel.java
@@ -1,4 +1,46 @@
 package mvc.model;
 
+import mvc.Karten.KarteiKarte;
+import mvc.Karten.KarteiKarten;
+
+
 public class WordleModel {
+    private KarteiKarte[] cards;
+    private int currentAnswer;
+    private int questions;
+    private int questionscorrect;
+    private long startTime,endTime;
+    public KarteiKarte[] startQuiz(KarteiKarten cards) {
+        startTime = System.currentTimeMillis();
+        currentAnswer = 0;
+        this.cards = cards.shuffle();
+        questions = 0;
+        questionscorrect = 0;
+        return this.cards;
+    }
+
+    public int[] endQuiz() {
+        endTime = System.currentTimeMillis();
+        int[] results = new int[3];
+        results[0] = questions;
+        results[1] = questionscorrect;
+        results[2] = (int) (endTime-startTime)/1000;
+        return results;
+    }
+    public boolean check(String answer) {
+        questions++;
+        System.out.println("checkmet" +cards[currentAnswer].getAntwort() +"answ"+ answer + cards[currentAnswer].isAntwort(answer));
+
+        if( cards[currentAnswer].isAntwort(answer)) {
+            System.out.println(cards[currentAnswer].getAntwort() +"answ"+ answer + cards[currentAnswer].isAntwort(answer));
+            this.questionscorrect++;
+            this.currentAnswer++;
+            return true;
+        }
+        else{
+            this.currentAnswer++;
+            return false;
+        }
+
+    }
 }
